3a8615b3be70140d50a004ec32e34cefd8db3928
errolgh <ewatsonrei@gmail.com>
2019-07-31 09:01:23 -0400

    waiting GET request

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,9 +1,24 @@
 class ForecastsController < ApplicationController
   def index
-    render json: Forecast.all
+    render json: Forecast.all.to_json({
+      :include => {
+        :days => {
+          except: [:created_at, :updated_at]
+        }},
+        except: [:created_at, :updated_at]
+    })
   end
 
   def show
-    render json: Forecast.find(params[:id])
+    render json: Forecast.find(params[:id]).to_json(
+      except: [:created_at, :updated_at]
+    )
   end
+
+  def create
+    # byebug
+    # puts "we tried to add a forecast"
+
+  end
+
 end
